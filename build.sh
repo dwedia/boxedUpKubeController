a4c99338de36d14fa99cbff8b401aaff4ad538d0
Dwedia <eric@bellaiche.net>
2025-12-07 16:24:46 +0100

    .gitignore and setenv file

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+podman build -f containerfile -t localhost/boxedupkubecontroller:latest
---
+++ configs/setenv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export CONTROL_PLANE_IP=10.0.0.20
+export CLUSTER_NAME=laptopserver
+KUBECONFIG=./alternative-kubeconfig
